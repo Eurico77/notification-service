e8f92e43e30055c198d503bf7f1499ea7a343d17
eurico77 <euricomagalhaes61@gmail.com>
2022-12-13 21:06:40 -0300

    (chore): create setup prisma

---
+++ prisma/migrations/20221213231153_create_notificatios/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "readAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Notification_recipientId_idx" ON "Notification"("recipientId");
