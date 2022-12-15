9c4f09a10a7b4470ca1e66b05234bbeba04b9709
eurico77 <euricomagalhaes61@gmail.com>
2022-12-15 07:44:12 -0300

    (chore): send a new notification usecase

--- src/app/use-cases/send-notification.ts
+++ src/app/use-cases/send-notification.ts
@@ -1,6 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
-import { NotificationRepository } from '../repositories/notification-repository';
+import { NotificationRepository } from '../contracts/repositories/notification-repository';
 
 interface SendNotificationRequest {
   recipientId: string;
@@ -12,6 +13,7 @@ interface SendNotificationResponse {
   notification: Notification;
 }
 
+@Injectable()
 export class SendNotification {
   constructor(private notificationRepository: NotificationRepository) {}
 
