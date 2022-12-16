a33c958a3340165bd13893eb13f698045c6e1948
eurico77 <euricomagalhaes61@gmail.com>
2022-12-16 08:41:52 -0300

    (chore): add methods in notification and adjust props

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -6,6 +6,7 @@ export interface NotificationProps {
   recipientId: string;
   content: Content;
   category: string;
+  cancelAt?: Date | null;
   readAt?: Date | null;
   createdAt: Date;
 }
@@ -14,8 +15,11 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
@@ -64,5 +68,21 @@ export class Notification {
 
   public get createdAt(): Date {
     return this.props.createdAt;
+  }
+
+  public cancel() {
+    this.props.cancelAt = new Date();
+  }
+
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
+  }
+
+  public get cancelAt(): Date | null | undefined {
+    return this.props.cancelAt;
   }
 }
