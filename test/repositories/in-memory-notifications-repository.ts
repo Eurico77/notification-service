8e2b17f60e20a2ffd3d5fbdd5c6cd896e3475c87
eurico77 <euricomagalhaes61@gmail.com>
2022-12-16 08:40:49 -0300

    (chore): create methods in repository in memory

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,8 +1,37 @@
-import { Notification } from '../../src/app/entities/notification';
-import { NotificationRepository } from '../../src/app/repositories/notification-repository';
+import { NotificationRepository } from '@app/contracts/repositories/notification-repository';
+import { Notification } from '@app/entities/notification';
 
 export class NotificationRepositoryInMemory implements NotificationRepository {
   public notifications: Notification[] = [];
+
+  async countManyByRecipientId(recipientId: string) {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (item) => item.recipientId === recipientId,
+    );
+  }
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) => notification.id === notificationId,
+    );
+
+    return notification ?? null;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
 
   async create(notification: Notification) {
     await this.notifications.push(notification);
